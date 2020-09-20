fea59ee1a811cc95fc0289d163000ffd05c3acb4
ChengQi930 <www.c981564325@163.com>
2020-09-20 23:48:37 +0800

    上传中央节点

--- src/main/java/cn/bizeron/dome/index.java
+++ src/main/java/cn/bizeron/dome/index.java
@@ -5,6 +5,7 @@ public class index {
 	public static void main(String[] args) {
        System.out.println("hello.git");
        System.out.println("Match 分支创建");
+       System.out.println("提交至中央节点");
 	}
 
 }
